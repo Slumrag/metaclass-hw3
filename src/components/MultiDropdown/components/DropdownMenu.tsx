51cfeec94c51c8d023b118a38050bc4545f371f0
Slumrag <mrgreenthecristal@yandex.ru>
2024-10-31 22:37:14 +0300

    style: :rotating_light: lint DropdownMenu

--- src/components/MultiDropdown/components/DropdownMenu.tsx
+++ src/components/MultiDropdown/components/DropdownMenu.tsx
@@ -1,34 +1,40 @@
-import React, { useEffect } from 'react';
-import {type Option} from '../MultiDropdown';
-import Text from '../../Text';
-import './DropdownMenu.css';
 import classNames from 'classnames';
+import React from 'react';
+import Text from 'components/Text';
+import { type Option } from '../MultiDropdown';
+import './DropdownMenu.css';
 
-type DropdownMenuProps = {
+export type DropdownMenuProps = {
   className?: string;
   options: Option[];
   value: Option[];
-  filterCb?:(opt:Option,index?:number,arr?:Option[])=>boolean;
-  onClick:(option:Option)=>void;
+  filterCb?: (opt: Option, index?: number, arr?: Option[]) => boolean;
+  onClick: (option: Option) => void;
 };
 
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ className,options,value,filterCb=()=>true,onClick }) => {
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ className, options, value, filterCb = () => true, onClick }) => {
+  const newOptions = value.length > 0 ? options : options.filter(filterCb);
 
-  const newOptions=value.length > 0 ? options:options.filter(filterCb);
-
-  return <menu className={classNames("DropdownMenu",className)} >
-        {
-          newOptions.length>0
-          ?
-          newOptions.map((e) => (
-            <li key={e.key} id={e.key} className={classNames('DropdownMenu__item',{DropdownMenu__item_selected:value.includes(e)})} onClick={()=>onClick(e)}>
-              <Text view='p-16'>{e.value}</Text>
-              </li>
-          ))
-          :
-          <Text color='secondary' view='p-16'>Пусто</Text>
-        }
-      </menu>;
+  return (
+    <menu className={classNames('DropdownMenu', className)}>
+      {newOptions.length > 0 ? (
+        newOptions.map((e) => (
+          <li
+            key={e.key}
+            id={e.key}
+            className={classNames('DropdownMenu__item', { DropdownMenu__item_selected: value.includes(e) })}
+            onClick={() => onClick(e)}
+          >
+            <Text view="p-16">{e.value}</Text>
+          </li>
+        ))
+      ) : (
+        <Text color="secondary" view="p-16">
+          Пусто
+        </Text>
+      )}
+    </menu>
+  );
 };
 
 export default DropdownMenu;

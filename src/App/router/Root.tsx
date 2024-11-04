252884807160d7e45911f99e06aa1f5e52733d3a
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-04 16:22:15 +0300

    feat: :construction_worker: redirect to kttsstudio on /

--- src/App/router/Root.tsx
+++ src/App/router/Root.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { Layout } from 'components/';
 import { DEFAULT_ORG } from 'configs/api';
 
 const Root = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   useEffect(() => {
-    navigate(DEFAULT_ORG);
+    if (location.pathname === '/') navigate(DEFAULT_ORG);
   }, []);
   return (
     <Layout>

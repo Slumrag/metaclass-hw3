095fe644c01a91597a6f4858d902b9332d3ae03c
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-04 21:02:35 +0300

    refactor: add error element to RepoPage path

--- src/App/router/router.tsx
+++ src/App/router/router.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-// import { typesafeBrowserRouter } from 'react-router-typesafe';
 import { MainPage, RepoPage, NotFoundPage } from 'App/pages';
 import { loader as mainPageLoader } from 'App/pages/MainPage/loader/loader';
 import { loader as repoPageLoader } from 'App/pages/RepoPage/loader/loader';
@@ -24,6 +23,7 @@ export const router = createBrowserRouter([
       },
       {
         path: ':org/:repo',
+        errorElement: <span>some err</span>,
         loader: repoPageLoader,
         element: <RepoPage />,
       },

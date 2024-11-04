9c0f6fbf80c247445cb2bd3ddb948f74944418bf
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-04 20:52:27 +0300

    refactor: :coffin: remove unused imports

--- src/App/pages/MainPage/MainPage.tsx
+++ src/App/pages/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLoaderData } from 'react-router-typesafe';
 

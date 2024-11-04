cdc59a4ca5eb9c3b1aca8f04a62367b4ebaf9575
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-05 01:58:06 +0300

    feat: :sparkles: add ProgressBar

---
+++ src/components/ProgressBar/index.ts
@@ -0,0 +1,2 @@
+export { default } from './ProgressBar';
+export type * from './ProgressBar';
--- src/components/index.ts
+++ src/components/index.ts
@@ -12,3 +12,4 @@ export { default as Layout } from './Layout';
 export { default as Header } from './Header';
 export { default as Container } from './Container';
 export { default as IconButton } from './IconButton';
+export { default as ProgressBar } from './ProgressBar';

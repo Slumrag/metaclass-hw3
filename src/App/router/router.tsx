10fa5ec705f9862f5db322d2a6c3e63cefe8746a
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-05 13:45:31 +0300

    feat: :sparkles: add basic ErrorPage

--- src/App/router/router.tsx
+++ src/App/router/router.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import { MainPage, RepoPage, NotFoundPage } from 'App/pages';
+import { MainPage, RepoPage, ErrorPage } from 'App/pages';
 import { loader as mainPageLoader } from 'App/pages/MainPage/loader/loader';
 import { loader as repoPageLoader } from 'App/pages/RepoPage/loader/loader';
 import Root from './Root';
 
 export const router = createBrowserRouter([
   {
-    path: '*',
-    element: <NotFoundPage />,
-  },
-  {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        errorElement: <span>some err</span>,
+        errorElement: <ErrorPage />,
         path: ':org',
         loader: mainPageLoader,
         element: <MainPage />,
       },
       {
         path: ':org/:repo',
-        errorElement: <span>some err</span>,
+        errorElement: <ErrorPage />,
         loader: repoPageLoader,
         element: <RepoPage />,
       },

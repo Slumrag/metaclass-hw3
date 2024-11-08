03eeead2bfa6d1e40bdeda5c54dcc38bef2dd2d4
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-08 13:25:45 +0300

    refactor: :recycle: remove duplicate schemas

---
+++ src/App/api/githubApi/types/schema/CodeOfConductSimple.ts
@@ -0,0 +1,10 @@
+/**
+ * Code of Conduct Simple
+ */
+export interface CodeOfConductSimple {
+  url: string;
+  key: string;
+  name: string;
+  html_url: string | null;
+  [k: string]: unknown;
+}
--- src/App/api/githubApi/types/schema/MinimalRepository.ts
+++ src/App/api/githubApi/types/schema/MinimalRepository.ts
@@ -1,3 +1,6 @@
+import { CodeOfConduct } from './CodeOfConduct';
+import { SimpleUser } from './SimpleUser';
+
 /**
  * Minimal Repository
  */
@@ -138,44 +141,5 @@ export interface MinimalRepository {
     };
     [k: string]: unknown;
   } | null;
-  [k: string]: unknown;
-}
-/**
- * A GitHub user.
- */
-export interface SimpleUser {
-  name?: string | null;
-  email?: string | null;
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string | null;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
-  starred_at?: string;
-  user_view_type?: string;
-  [k: string]: unknown;
-}
-/**
- * Code Of Conduct
- */
-export interface CodeOfConduct {
-  key: string;
-  name: string;
-  url: string;
-  body?: string;
-  html_url: string | null;
   [k: string]: unknown;
 }

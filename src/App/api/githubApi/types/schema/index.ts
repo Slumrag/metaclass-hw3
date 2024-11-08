2dde3a96189ef80e9b5761a2507ef1d268f93d39
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-08 22:03:25 +0300

    refactor: update schema exports

--- src/App/api/githubApi/types/schema/index.ts
+++ src/App/api/githubApi/types/schema/index.ts
@@ -1,8 +1,8 @@
 export type { MinimalRepository as MinimalRepositoryApi } from './MinimalRepository';
-export type {
-  FullRepository as FullRepositoryApi,
-  LicenseSimple as LicenseSimpleApi,
-  SimpleUser as SimpleUserApi,
-} from './FullRepository';
+export type { FullRepository as FullRepositoryApi } from './FullRepository';
+export type { LicenseSimple as LicenseSimpleApi } from './LicenseSimple';
+export type { SimpleUser as SimpleUserApi } from './SimpleUser';
+export type { CodeOfConduct as CodeOfConductApi } from './CodeOfConduct';
+export type { CodeOfConductSimple as CodeOfConductSimpleApi } from './CodeOfConductSimple';
 export type { ContentFile as ContentFileApi } from './ContentFile';
 export type { RepositoryLanguages as RepositoryLanguagesApi } from './RepositoryLanguages';

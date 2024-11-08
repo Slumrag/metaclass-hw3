f429c4d310b7895203d8d3bf5a8e9634de6cadb9
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-08 13:43:56 +0300

    refactor: :fire: remove Layout.module.scss

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,10 @@
-import classNames from 'classnames';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import Header from 'components/Header';
-import style from './Layout.module.scss';
 
 const Layout: React.FC<React.ComponentProps<'div'>> = observer(({ className, children, ...props }) => {
   return (
-    <div className={classNames(style.layout, className)} {...props}>
+    <div className={className} {...props}>
       <Header />
       <main>{children}</main>
     </div>

69a795c73f8fd6f8191c42ef2eeb0b4b2203915b
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-10 23:09:23 +0300

    feat: :sparkles: update Orgainzation store

--- src/store/OrganizationStore/OrganizationStore.ts
+++ src/store/OrganizationStore/OrganizationStore.ts
@@ -1,21 +1,39 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 import { getOrgRepos, OrgReposOptions } from 'App/api';
+import { TYPE_OPTIONS } from 'App/api/githubApi/types';
+import RootStore from 'store/RootStore/RootStore';
 import { MinimalRepositoryModel, normalizeMinimalRepository, SimpleUserModel } from 'store/models';
 import { META } from 'utils/const';
+import { parseGitHubLinkHeader, clamp } from 'utils/helpers';
+import { IPaginationStore } from 'utils/types';
 
-type PrivateFields = '_repos' | '_meta' | '_organization';
+type PrivateFields = '_repos' | '_meta' | '_organization' | '_currentPage' | '_pages' | '_perPage' | '_type';
 
-class OrganizationStore {
-  private _repos: MinimalRepositoryModel[] = [];
+class OrganizationStore implements IPaginationStore<MinimalRepositoryModel> {
+  private _data: MinimalRepositoryModel[] = [];
   private _meta: META = META.INITIAL;
   private _organization: SimpleUserModel | null = null;
+  private _currentPage: number = 1;
+  private _pages: number | null = null;
+  private _perPage: number = 3;
+  private _type: `${TYPE_OPTIONS}` = TYPE_OPTIONS.all;
+  readonly root: RootStore;
 
-  constructor() {
-    makeAutoObservable<OrganizationStore, PrivateFields>(this);
+  constructor(root: RootStore) {
+    makeAutoObservable<OrganizationStore, PrivateFields>(this, {
+      _meta: true,
+      _currentPage: true,
+      _organization: true,
+      _pages: true,
+      _perPage: true,
+      _repos: true,
+      _type: true,
+    });
+    this.root = root;
   }
 
-  get repos() {
-    return this._repos;
+  get data() {
+    return this._data;
   }
   get meta() {
     return this._meta;
@@ -23,15 +41,73 @@ class OrganizationStore {
   get organization() {
     return this._organization;
   }
+  get currentPage() {
+    return this._currentPage;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+  get perPage() {
+    return this._perPage;
+  }
+  get name() {
+    return this.organization?.name;
+  }
+  get type() {
+    return this._type;
+  }
+
+  set type(type: `${TYPE_OPTIONS}` | undefined) {
+    if (type !== undefined) {
+      this._type = type;
+    }
+  }
+
+  set currentPage(page: number) {
+    this._currentPage = clamp(1, page, this.pages!);
+  }
+
+  public async goToPage(page: number) {
+    this.currentPage = page;
+    await this.getRepos(this.name!, { page: this.currentPage });
+  }
 
   public async getRepos(org: string, options?: OrgReposOptions) {
-    this._meta = META.LOADING;
+    if (!org) {
+      return;
+    }
+    runInAction(() => {
+      this._meta = META.LOADING;
+    });
+
     try {
-      const response = await getOrgRepos(org, options);
+      const defaultOptions: OrgReposOptions = { type: this.type, per_page: this.perPage, page: this.currentPage };
+
+      const newOptions: OrgReposOptions = options !== undefined ? { ...defaultOptions, ...options } : defaultOptions;
+
+      const response = await getOrgRepos(org, newOptions);
+
       runInAction(() => {
         this._meta = META.SUCCESS;
-        this._repos = response.data.map(normalizeMinimalRepository);
-        this._organization = this.repos[0]?.owner;
+
+        this.type = options?.type;
+
+        this._perPage = options?.per_page ?? this._perPage;
+        this.currentPage = options?.page ?? this.currentPage;
+
+        this._data = response.data.map(normalizeMinimalRepository);
+
+        this._organization = this.data[0]?.owner;
+
+        const linkParams = response.headers?.link ? parseGitHubLinkHeader(response.headers?.link) : null;
+
+        if (linkParams) {
+          const page = linkParams.last.searchParams.get('page');
+          this._pages = page ? Number(page) : null;
+        } else {
+          this._pages = 1;
+        }
       });
     } catch (error) {
       this._meta = META.ERROR;

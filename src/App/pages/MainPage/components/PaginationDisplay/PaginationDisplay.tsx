58359fdf9b95de0ecb3b685ece8aa47864f350b2
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-24 23:04:13 +0300

    refactor: :recycle: add messege for empty search

--- src/App/pages/MainPage/components/PaginationDisplay/PaginationDisplay.tsx
+++ src/App/pages/MainPage/components/PaginationDisplay/PaginationDisplay.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ErrorText } from 'components/';
 import { rootStore } from 'store/';
+import { META } from 'utils/';
 import PaginationControls from '../PaginationControls';
-import RepoCardDisplay from '../RepoCardDisplay';
 import style from './PaginationDisplay.module.scss';
+
+const RepoCardDisplay = React.lazy(() => import('../RepoCardDisplay'));
 
 export type PaginationDisplayProps = React.ComponentProps<'div'>;
 
@@ -25,11 +27,11 @@ const PaginationDisplay: React.FC<PaginationDisplayProps> = observer(() => {
 
   return (
     <>
-      {organization.data.length > 0 ? (
-        <RepoCardDisplay data={organization.data} onClick={handleRepo} />
-      ) : (
-        <ErrorText>Not found</ErrorText>
+      <RepoCardDisplay data={organization.data} onClick={handleRepo} />
+      {(organization.meta === META.SUCCESS && organization.data.length) === 0 && (
+        <ErrorText>No repositories were found</ErrorText>
       )}
+
       {organization.pages && organization.pages > 1 && (
         <PaginationControls
           className={style.pagination}

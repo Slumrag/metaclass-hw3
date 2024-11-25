0ad903f5d8059b810933246070dd39ae4157b023
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-25 15:42:28 +0300

    feat: :sparkles: make markdown lazy

--- src/App/pages/RepoPage/components/DisplayReadMe/DisplayReadMe.tsx
+++ src/App/pages/RepoPage/components/DisplayReadMe/DisplayReadMe.tsx
@@ -1,14 +1,15 @@
 import classNames from 'classnames';
 import { observer } from 'mobx-react-lite';
-import React, { useEffect, useState } from 'react';
-import Markdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import React, { Suspense, useEffect, useState } from 'react';
 import { oneLight as highlightStyle } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 import { downloadReadMe } from 'App/api';
-import { Text } from 'components/';
+import { Loader, Skeleton, Text } from 'components/';
 import style from './DisplayReadMe.module.scss';
+
+const Markdown = React.lazy(() => import('react-markdown'));
+const SyntaxHighlighter = React.lazy(() => import('react-syntax-highlighter/dist/esm/prism'));
 
 export type DisplayReadMeProps = {
   /** ссылка на файл */
@@ -36,34 +37,40 @@ const DisplayReadMe: React.FC<DisplayReadMeProps> = observer(({ src, className }
           <Text className={style.title} weight="bold">
             README.md
           </Text>
-          <Markdown
-            className={style.markdown}
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              code(props) {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                const { children, className, node, ...rest } = props;
-                const match = /language-(\w+)/.exec(className || '');
-                return match ? (
-                  <SyntaxHighlighter
-                    // {...rest}
-                    PreTag="div"
-                    language={match[1]}
-                    style={highlightStyle}
-                  >
-                    {String(children).replace(/\n$/, '')}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code {...rest} className={className}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-          >
-            {markdown}
-          </Markdown>
+          <div className={style.markdownWrapper}>
+            <Suspense fallback={<Loader size="l" />}>
+              <Markdown
+                className={style.markdown}
+                remarkPlugins={[remarkGfm]}
+                rehypePlugins={[rehypeRaw]}
+                components={{
+                  code(props) {
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    const { children, className, node, ...rest } = props;
+                    const match = /language-(\w+)/.exec(className || '');
+                    return match ? (
+                      <Suspense fallback={<Skeleton height={56} />}>
+                        <SyntaxHighlighter
+                          // {...rest}
+                          PreTag="div"
+                          language={match[1]}
+                          style={highlightStyle}
+                        >
+                          {String(children).replace(/\n$/, '')}
+                        </SyntaxHighlighter>
+                      </Suspense>
+                    ) : (
+                      <code {...rest} className={className}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              >
+                {markdown}
+              </Markdown>
+            </Suspense>
+          </div>
         </div>
       )}
     </>

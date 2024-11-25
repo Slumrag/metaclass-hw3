83855cd3f51e4458a56b15689a3a37c062375344
Slumrag <mrgreenthecristal@yandex.ru>
2024-11-25 16:29:15 +0300

    refactor: :recycle: extract RepoNav to a component

--- src/App/pages/RepoPage/RepoPage.tsx
+++ src/App/pages/RepoPage/RepoPage.tsx
@@ -1,7 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ArrowLeftIcon, Avatar, Container, IconButton, Text } from 'components/';
+import { Container } from 'components/';
 import { RepoPageStore, rootStore } from 'store/';
 import { useLocalStore } from 'utils/';
 import DisplayReadMe from './components/DisplayReadMe';
@@ -21,26 +20,12 @@ const RepoPage: React.FC = observer(() => {
     }
   }, []);
 
-  const navigate = useNavigate();
   return (
-    <div className={style.wrapper}>
       <Container className={style.container} align="start">
-        <div className={style.title}>
-          <IconButton
-            className={style.backButton}
-            variant="transparent"
-            icon={<ArrowLeftIcon />}
-            onClick={() => navigate(-1)}
-          />
-          <Avatar src={repoStore.repo?.owner.avatarUrl ?? '/placeholder.png'} alt="" variant="rounded" />
-          <Text tag={'h2'} view="title" className={style.text}>
-            {repoStore.repo?.name}
-          </Text>
-        </div>
+      <RepoNav avatarUrl={repoStore.repo?.owner.avatarUrl} name={repoStore.repo?.name} />
         {repoStore.repo && <RepoInfo className={style.info} repo={repoStore.repo} />}
         {repoStore.readme && <DisplayReadMe src={repoStore.readme} className={style.markdown} />}
       </Container>
-    </div>
   );
 });
 
